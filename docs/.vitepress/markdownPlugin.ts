837c22a394224830763932b6ffad065cfa8f14f8
itxh <892457803@qq.com>
2024-03-21 11:17:20 +0800

    更新：图片显示样式

--- docs/.vitepress/markdownPlugin.ts
+++ docs/.vitepress/markdownPlugin.ts
@@ -1,4 +1,10 @@
 import MarkdownIt from "markdown-it";
+/**
+ * 参考文档
+ * [markdown-it 14.1.0 API documentation](https://markdown-it.github.io/markdown-it/)
+ * [luoleiorg/docs/.vitepress/markdownPlugin.ts](https://github.com/foru17/luoleiorg/blob/main/docs/.vitepress/markdownPlugin.ts)
+ * @param md 
+ */
 const markdownImagePlugin: MarkdownIt.PluginSimple = (md) => {
   md.renderer.rules.image = (tokens, idx, options, env, self) => {
     const token = tokens[idx];
